83e00487cb6b576488754b962344231ee4306d47
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-08-17 10:26:03 +0300

    Make first part of Nav Bar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,32 @@
-import { VStack, HStack, Input, Image, Text, Box } from "@chakra-ui/react";
-import logo from "../img/img/beerdome-logo-black.png";
+import {
+  Box,
+  HStack,
+  Image,
+  Input,
+  Show,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import logo from "../img/img/beerdome-logo-black.png";
 
 const NavBar = () => {
   return (
     <Box
       display="flex"
       alignItems="center"
-      justifyContent={{ base: "center", lg: "space-between" }}
+      justifyContent="space-between"
+      flexWrap="wrap"
       paddingY="20px"
-      paddingX={{ base: "unset", md: "2%", xl: "12%" }}
+      paddingX={{ base: "2%", md: "2%", xl: "14%" }}
       width="100%"
     >
+      <Show below="lg">
+        <GiHamburgerMenu size={32} />
+      </Show>
       <Input
         display={{ base: "none", lg: "flex" }}
         placeholder="Search..."
@@ -27,8 +41,13 @@ const NavBar = () => {
       />
       <Image
         src={logo}
-        marginLeft={{ base: "20px", lg: "-120px" }}
+        alt="BEERDOME"
+        marginLeft={{ base: "20px", lg: "-120px", "2xl": "-180px" }}
         marginRight="20px"
+        maxWidth="100%"
+        height="auto"
+        objectFit="cover"
+        width={{ base: "180px", sm: "240px" }}
       ></Image>
       <HStack
         display={{ base: "none", lg: "flex" }}
@@ -48,6 +67,9 @@ const NavBar = () => {
           <Text>Cart</Text>
         </VStack>
       </HStack>
+      <Show below="lg">
+        <HiMagnifyingGlass size={32} />
+      </Show>
     </Box>
   );
 };
