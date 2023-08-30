92cb4d302ae1ef133ee630799283e72354f86c3e
Ivan Sheva <sevcenkoivan673@gmail.com>
2023-08-30 09:23:28 +0300

    Add two more style products

--- src/data/styleProducts.js
+++ src/data/styleProducts.js
@@ -1,5 +1,7 @@
 import style1 from "../img/img/style1.jpg";
 import style2 from "../img/img/style2.jpg";
+import style3 from "../img/img/style3.jpg";
+import style4 from "../img/img/style4.jpg";
 
 let lastId = 0;
 
@@ -23,6 +25,26 @@ const styleProducts = [
       "Complimenting notes of Madeira with the sweetness of roasted malts, this robust Whiskey Cask barrel-aged Imperial Stout is rich with notes of chocolate and liquorice. Hopped with Willamette and Magnum. CROWLER! CANNED 10-8, BEST BEFORE 21-9",
     rating: 4.6,
     img: style2,
+  },
+  {
+    id: ++lastId,
+    brand: "Toppling Goliath",
+    name: "Toppling Goliath - Double Dry Hop King Sue",
+    price: 10.99,
+    description:
+      "Citra hops give King Sue its bold flavors of mango, orange and pineapple. This double dry hopped version packs even more flavor into each can.",
+    rating: 4.8,
+    img: style3,
+  },
+  {
+    id: ++lastId,
+    brand: "Fremont",
+    name: "Fremont - Brew 6000 (2022)",
+    price: 36.99,
+    description:
+      "This English-style barleywine ale uses floor-malted English barley, Noble hops and is aged in 10-15 year-old, single use bourbon barrels for 16 months to bring you a complex and subtle craft beer flavor experience. Brewed to celebrate the 6000th brew!",
+    rating: 4.8,
+    img: style4,
   },
 ];
 
